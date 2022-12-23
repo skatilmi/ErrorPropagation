133a75d641c8fa1ac8c741e0a78c756a82998224
samet <skatilmi@students.uni-mainz.de>
2022-12-23 13:58:01 +0100

    fixed bug in displayed relative error

--- Error_minimal_for_web.py
+++ Error_minimal_for_web.py
@@ -201,25 +201,17 @@ class Error_propagation:
     def get_python_represenaion(self, expression, nosqrt=False, delta=False, relative_to_f=False):
         delta = 'Delta_' if delta else ''
         function_name = self.function_name
-        if not nosqrt:
-            expression = sp.sqrt(expression)
         p = str(expression).replace('\\', '').replace('Delta ', 'Delta_')
         c: str = Code(sp.Symbol('xxx'), p).raw_code[1:-1]
 
         free_symbols: List[str] = [str(i).replace('\\', '').replace('Delta ', 'Delta_') for i in expression.free_symbols]
-        free_symbols.sort(key=lambda x: len(x), reverse=False)
-        e = []
-
-        for i in free_symbols:
-            if str(i) != function_name:
-                e.append(i)
-            else:
-                e.append(f'{function_name}_data')
-        free_symbols = e
+        free_symbols = [i if str(i) != function_name else f'{function_name}_data' for i in free_symbols]
+        free_symbols.sort()
 
         if relative_to_f:
-            idx = c.find('*')
-            c = f'{c[:idx-1]}f_data{c[idx:]}'
+            idx = c.find(function_name)
+            c = f'{c[:idx]}{function_name}_data{c[idx+1:]}'
+
         return warp_in_function_syntax(
             f'{delta}{function_name}', free_symbols, c)
 
